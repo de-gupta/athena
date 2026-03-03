d78229c8e9700220e888e623242608204f52cf10
de-gupta <de-gupta>
2026-03-03 14:16:43 +0100

    euclidean domain

--- src/main/java/de/gupta/commons/utility/math/algebra/ring/DivisionResult.java
+++ src/main/java/de/gupta/commons/utility/math/algebra/ring/DivisionResult.java
@@ -2,4 +2,8 @@ package de.gupta.commons.utility.math.algebra.ring;
 
 public record DivisionResult<E>(E quotient, E remainder)
 {
+	public static <E> DivisionResult<E> of(E quotient, E remainder)
+	{
+		return new DivisionResult<>(quotient, remainder);
+	}
 }
