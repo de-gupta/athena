9408c004c87044f80aa2afde08a9fe8ba8f712d9
de-gupta <de-gupta>
2026-03-11 10:19:11 +0100

    added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,26 @@
+module de.gupta.athena
+{
+	exports de.gupta.commons.utility.collection;
+	exports de.gupta.commons.utility.comparison;
+
+	exports de.gupta.commons.utility.io.path;
+	exports de.gupta.commons.utility.io.read;
+	exports de.gupta.commons.utility.io.write;
+
+	exports de.gupta.commons.utility.javaLanguage.classes;
+	exports de.gupta.commons.utility.javaLanguage.packages;
+	exports de.gupta.commons.utility.javaLanguage.code;
+	exports de.gupta.commons.utility.javaLanguage.comments;
+
+	exports de.gupta.commons.utility.map;
+	exports de.gupta.commons.utility.map.enumMap;
+
+	exports de.gupta.commons.utility.string;
+	exports de.gupta.commons.utility.security;
+
+	exports de.gupta.commons.utility.math.prefix;
+	exports de.gupta.commons.utility.math.algebra.algebraicGroup;
+	exports de.gupta.commons.utility.math.algebra.ring;
+
+	requires aletheia;
+}
