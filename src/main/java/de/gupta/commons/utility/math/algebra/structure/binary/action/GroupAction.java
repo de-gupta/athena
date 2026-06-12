fbb8fe375736431ade380f228e9ff32cae00b449
de-gupta <de-gupta>
2026-06-12 10:38:08 +0200

    other actions

---
+++ src/main/java/de/gupta/commons/utility/math/algebra/structure/binary/action/GroupAction.java
@@ -0,0 +1,7 @@
+package de.gupta.commons.utility.math.algebra.structure.binary.action;
+
+import de.gupta.commons.utility.math.algebra.structure.binary.GroupStructure;
+
+public interface GroupAction<S, X> extends MonoidAction<S, X>, GroupStructure<S>
+{
+}
