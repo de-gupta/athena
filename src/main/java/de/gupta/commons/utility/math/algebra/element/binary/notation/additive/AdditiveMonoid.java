3a0ad50574f0802d43e66d349193fd7c1a2a7f07
de-gupta <de-gupta>
2026-06-17 09:39:53 +0200

    removed misleading overload

--- src/main/java/de/gupta/commons/utility/math/algebra/element/binary/notation/additive/AdditiveMonoid.java
+++ src/main/java/de/gupta/commons/utility/math/algebra/element/binary/notation/additive/AdditiveMonoid.java
@@ -1,15 +1,5 @@
 package de.gupta.commons.utility.math.algebra.element.binary.notation.additive;
 
-import de.gupta.aletheia.collection.folding.Loom;
-
-import java.util.Objects;
-
 public interface AdditiveMonoid<E extends AdditiveMonoid<E>> extends AdditiveSemigroup<E>, Zero<E>
 {
-	@Override
-	default E addAll(final Loom<E> others)
-	{
-		Objects.requireNonNull(others, "others");
-		return others.weave(zero(), AdditiveSemigroup::add);
-	}
 }
