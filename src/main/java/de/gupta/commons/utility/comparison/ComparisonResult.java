670bbbba79417cf320bdee1644e4135a50d1454f
de-gupta <de-gupta>
2026-06-18 13:44:23 +0200

    utility methods on comparison types

--- src/main/java/de/gupta/commons/utility/comparison/ComparisonResult.java
+++ src/main/java/de/gupta/commons/utility/comparison/ComparisonResult.java
@@ -4,5 +4,10 @@ public enum ComparisonResult
 {
 	LESS_THAN,
 	EQUAL,
-	GREATER_THAN
+	GREATER_THAN;
+
+	public static ComparisonResult fromComparatorResult(final int comparatorResult)
+	{
+		return comparatorResult < 0 ? LESS_THAN : comparatorResult > 0 ? GREATER_THAN : EQUAL;
+	}
 }
--- src/main/java/de/gupta/commons/utility/comparison/ComparisonType.java
+++ src/main/java/de/gupta/commons/utility/comparison/ComparisonType.java
@@ -1,11 +1,62 @@
 package de.gupta.commons.utility.comparison;
 
+import java.util.Comparator;
+
 public enum ComparisonType
 {
-	LESS_THAN,
-	LESS_THAN_OR_EQUAL,
-	EQUAL,
-	GREATER_THAN_OR_EQUAL,
-	GREATER_THAN,
+	LESS_THAN
+			{
+				@Override
+				public boolean isCompatibleWith(final ComparisonResult result)
+				{
+					return result == ComparisonResult.LESS_THAN;
+				}
+			},
+	LESS_THAN_OR_EQUAL
+			{
+				@Override
+				public boolean isCompatibleWith(final ComparisonResult result)
+				{
+					return result == ComparisonResult.LESS_THAN || result == ComparisonResult.EQUAL;
+				}
+			},
+	EQUAL
+			{
+				@Override
+				public boolean isCompatibleWith(final ComparisonResult result)
+				{
+					return result == ComparisonResult.EQUAL;
+				}
+			},
+	GREATER_THAN_OR_EQUAL
+			{
+				@Override
+				public boolean isCompatibleWith(final ComparisonResult result)
+				{
+					return result == ComparisonResult.GREATER_THAN || result == ComparisonResult.EQUAL;
+				}
+			},
+	GREATER_THAN
+			{
+				@Override
+				public boolean isCompatibleWith(final ComparisonResult result)
+				{
+					return result == ComparisonResult.GREATER_THAN;
+				}
+			},
 	NOT_EQUAL
+			{
+				@Override
+				public boolean isCompatibleWith(final ComparisonResult result)
+				{
+					return result != ComparisonResult.EQUAL;
+				}
+			};
+
+	public <T> boolean compare(final T value, final T threshold, final Comparator<T> comparator)
+	{
+		return isCompatibleWith(ComparisonResult.fromComparatorResult(comparator.compare(value, threshold)));
+	}
+
+	public abstract boolean isCompatibleWith(final ComparisonResult result);
 }
