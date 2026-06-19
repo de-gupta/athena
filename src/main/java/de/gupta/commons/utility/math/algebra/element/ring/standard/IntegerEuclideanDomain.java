b3dbee7c853580ec4e426b7903826541b06812f9
de-gupta <de-gupta>
2026-06-19 14:47:54 +0200

    cleanup

--- src/main/java/de/gupta/commons/utility/math/algebra/element/ring/standard/IntegerEuclideanDomain.java
+++ src/main/java/de/gupta/commons/utility/math/algebra/element/ring/standard/IntegerEuclideanDomain.java
@@ -2,13 +2,16 @@ package de.gupta.commons.utility.math.algebra.element.ring.standard;
 
 import de.gupta.commons.utility.math.algebra.element.ring.EuclideanDomain;
 import de.gupta.commons.utility.math.algebra.structure.ring.DivisionResult;
+import de.gupta.commons.utility.math.algebra.structure.ring.standard.LongEuclideanDomainStructure;
 
 public record IntegerEuclideanDomain(long value) implements EuclideanDomain<IntegerEuclideanDomain>
 {
+	private final static LongEuclideanDomainStructure canonicalStructure = LongEuclideanDomainStructure.INSTANCE;
+
 	@Override
 	public IntegerEuclideanDomain negate()
 	{
-		return of(Math.negateExact(value));
+		return of(canonicalStructure.negate(value));
 	}
 
 	public static IntegerEuclideanDomain of(final long value)
@@ -19,42 +22,42 @@ public record IntegerEuclideanDomain(long value) implements EuclideanDomain<Inte
 	@Override
 	public IntegerEuclideanDomain zero()
 	{
-		return of(0);
+		return of(canonicalStructure.zero());
 	}
 
 	@Override
 	public IntegerEuclideanDomain one()
 	{
-		return of(1);
+		return of(canonicalStructure.one());
 	}
 
 	@Override
 	public IntegerEuclideanDomain multiply(final IntegerEuclideanDomain other)
 	{
-		return of(Math.multiplyExact(value, other.value));
+		return of(canonicalStructure.multiply(value, other.value));
 	}
 
 	@Override
 	public IntegerEuclideanDomain add(final IntegerEuclideanDomain other)
 	{
-		return of(Math.addExact(value, other.value));
+		return of(canonicalStructure.add(value, other.value));
 	}
 
 	@Override
 	public long norm()
 	{
-		return Math.absExact(value);
+		return canonicalStructure.norm(value);
 	}
 
 	@Override
 	public DivisionResult<IntegerEuclideanDomain> divideWithRemainder(final IntegerEuclideanDomain divisor)
 	{
-		return DivisionResult.of(of(Math.floorDiv(value, divisor.value)), of(Math.floorMod(value, divisor.value)));
+		return canonicalStructure.divideWithRemainder(value, divisor.value()).map(IntegerEuclideanDomain::of);
 	}
 
 	@Override
 	public boolean isZero()
 	{
-		return value == 0;
+		return canonicalStructure.isZero(value);
 	}
 }
