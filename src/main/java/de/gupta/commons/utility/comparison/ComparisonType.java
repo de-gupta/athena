22fbd02a58e29a50bb79b35af26696f36a6faaa0
de-gupta <de-gupta>
2026-06-19 10:20:57 +0200

    internal cleanup

--- src/main/java/de/gupta/commons/utility/comparison/ComparisonType.java
+++ src/main/java/de/gupta/commons/utility/comparison/ComparisonType.java
@@ -1,62 +1,31 @@
 package de.gupta.commons.utility.comparison;
 
 import java.util.Comparator;
+import java.util.Set;
 
 public enum ComparisonType
 {
-	LESS_THAN
-			{
-				@Override
-				public boolean isCompatibleWith(final ComparisonResult result)
-				{
-					return result == ComparisonResult.LESS_THAN;
-				}
-			},
-	LESS_THAN_OR_EQUAL
-			{
-				@Override
-				public boolean isCompatibleWith(final ComparisonResult result)
-				{
-					return result == ComparisonResult.LESS_THAN || result == ComparisonResult.EQUAL;
-				}
-			},
-	EQUAL
-			{
-				@Override
-				public boolean isCompatibleWith(final ComparisonResult result)
-				{
-					return result == ComparisonResult.EQUAL;
-				}
-			},
-	GREATER_THAN_OR_EQUAL
-			{
-				@Override
-				public boolean isCompatibleWith(final ComparisonResult result)
-				{
-					return result == ComparisonResult.GREATER_THAN || result == ComparisonResult.EQUAL;
-				}
-			},
-	GREATER_THAN
-			{
-				@Override
-				public boolean isCompatibleWith(final ComparisonResult result)
-				{
-					return result == ComparisonResult.GREATER_THAN;
-				}
-			},
-	NOT_EQUAL
-			{
-				@Override
-				public boolean isCompatibleWith(final ComparisonResult result)
-				{
-					return result != ComparisonResult.EQUAL;
-				}
-			};
+	LESS_THAN(Set.of(ComparisonResult.LESS_THAN)),
+	LESS_THAN_OR_EQUAL(Set.of(ComparisonResult.LESS_THAN, ComparisonResult.EQUAL)),
+	EQUAL(Set.of(ComparisonResult.EQUAL)),
+	GREATER_THAN_OR_EQUAL(Set.of(ComparisonResult.GREATER_THAN, ComparisonResult.EQUAL)),
+	GREATER_THAN(Set.of(ComparisonResult.GREATER_THAN)),
+	NOT_EQUAL(Set.of(ComparisonResult.LESS_THAN, ComparisonResult.GREATER_THAN));
+
+	private final Set<ComparisonResult> compatibleResults;
 
 	public <T> boolean compare(final T value, final T threshold, final Comparator<T> comparator)
 	{
 		return isCompatibleWith(ComparisonResult.fromComparatorResult(comparator.compare(value, threshold)));
 	}
 
-	public abstract boolean isCompatibleWith(final ComparisonResult result);
+	public boolean isCompatibleWith(final ComparisonResult result)
+	{
+		return compatibleResults.contains(result);
+	}
+
+	ComparisonType(final Set<ComparisonResult> compatibleResults)
+	{
+		this.compatibleResults = compatibleResults;
+	}
 }
